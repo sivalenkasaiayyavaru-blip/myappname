b1fe75a54ac06e719f156282912f9d95f718bf4d
Sai Ayyavaru <sivalenkasaiayyavaru@gmail.com>
2025-12-08 00:35:39 +0530

    ain

--- config/db.js
+++ config/db.js
@@ -63,24 +63,54 @@
 // module.exports = connection;
 
 
-const mysql = require('mysql2');
-require('dotenv').config();
+// const mysql = require('mysql2');
+// require('dotenv').config();
 
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+//   ssl: {rejectUnauthorized:true}
+// });
+
+// connection.connect(err => {
+//   if (err) {
+//     console.error('❌ DB connection failed:', err.message);
+//   } else {
+//     console.log('✅ Connected to DB successfully!');
+//   }
+// });
+
+// module.exports = connection;
+
+
+
+
+const mysql = require("mysql2");
+
+require("dotenv").config();
+
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  ssl: {rejectUnauthorized:true}
+  ssl: { rejectUnauthorized: true },
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-connection.connect(err => {
+pool.getConnection((err, connection) => {
   if (err) {
-    console.error('❌ DB connection failed:', err.message);
+    console.error("❌ DB connection failed:", err.message);
   } else {
-    console.log('✅ Connected to DB successfully!');
+    console.log("✅ Connected to DB successfully!");
+    connection.release();
   }
 });
 
-module.exports = connection;
+module.exports = pool;
